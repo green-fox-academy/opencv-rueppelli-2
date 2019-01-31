7e945a4b71479bb9fdd5148c0e60307577ba5d51
Rapcsak Andi <escalier81@gmail.com>
2019-01-31 13:47:02 +0100

    OP2-4 Blur the image

--- Computer_Vision/main.cpp
+++ Computer_Vision/main.cpp
@@ -8,22 +8,33 @@
 
 #define NAME "Computer Vision"
 
+cv::Mat image;
+cv::Mat output;
+
+int sliderMax = 15;
+int kernelSize = 1;
+
+static void onTrackbar(int, void *);
+static void onTrackbar2(int, void *);
+
 int main(int argc, char** argv) {
 
     cv::String imagePath("../img/balls12.jpg");
+    image = cv::imread(imagePath, cv::IMREAD_COLOR);
 
     if(argc > 1) {
         imagePath = argv[1];
     }
 
-    cv::Mat image = cv::imread(imagePath, cv::IMREAD_COLOR);
     if(image.empty()) {
         std::cout << "Sorry baby, I can't find the image." << std::endl ;
         return -1;
     }
 
     namedWindow(NAME, cv::WND_PROP_FULLSCREEN);
-    imshow(NAME, image);
+    cv::createTrackbar("Gaussian Blur", NAME, &kernelSize, sliderMax, onTrackbar);
+    cv::createTrackbar("Median Blur", NAME, &kernelSize, sliderMax, onTrackbar2);
+
     cv::waitKey(0);
 
     cv::Mat circles = detectCircle(image);
@@ -34,3 +45,13 @@ int main(int argc, char** argv) {
     return 0;
 }
 
+static void onTrackbar(int, void *)
+{
+    cv::GaussianBlur(image, output, cv::Size(2*kernelSize+1, 2*kernelSize+1), 0,0);
+    imshow(NAME, output);
+}
+static void onTrackbar2(int, void *)
+{
+    cv::medianBlur(image, output, 2*kernelSize + 1);
+    imshow(NAME, output);
+}
