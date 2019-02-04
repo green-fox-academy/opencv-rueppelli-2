1381fb803f36ecb284eebb1620ec97a7854f7ca1
Dextyh <tmark.boy001@gmail.com>
2019-02-04 13:35:29 +0100

    Fixed Google Test

--- gtest/unit_test.cpp
+++ gtest/unit_test.cpp
@@ -4,22 +4,22 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
-#include "../src/detect_circle.h"
+#include "detect_circle.h"
 
 TEST(DetectCircles, noCircle)
 {
-    cv::Mat image = cv::imread("../../img/noCircle.jpg");
+    cv::Mat image = cv::imread("../../img/noCircle.jpg", cv::IMREAD_GRAYSCALE);
     EXPECT_EQ(detectCircle(image), 0);
 }
 
 TEST(DetectCircles, numerousCircles)
 {
-    cv::Mat image = cv::imread("../../img/ball.jpg");
+    cv::Mat image = cv::imread("../../img/ball.jpg", cv::IMREAD_GRAYSCALE);
     EXPECT_EQ(detectCircle(image), 3);
 }
 
 TEST(DetectCircles, overlayCircles)
 {
-    cv::Mat image = cv::imread("../../img/balls11.jpg");
+    cv::Mat image = cv::imread("../../img/balls11.jpg", cv::IMREAD_GRAYSCALE);
     EXPECT_EQ(detectCircle(image), 3);
 }
