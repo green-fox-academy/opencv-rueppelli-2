5383b29dbb7b3cd09093b0eac38cdf34786936be
FanniTakax <tkcsfni@gmail.com>
2019-02-05 13:53:17 +0100

    OP2-12 Selection sort

---
+++ src/sort_functions.cpp
@@ -0,0 +1,101 @@
+#include "sort_functions.h"
+
+std::vector<int> selectionSort(std::vector<int> &numbers, int o)
+{
+    int i, j, mindex;
+    if(o == 1){
+        for (i = 0; i < numbers.size(); ++i) {
+            mindex = i;
+            for (j = i + 1; j < numbers.size(); ++j) {
+                if(numbers[mindex] > numbers[j]){
+                    mindex = j;
+                }
+            }
+            int swap = numbers[i];
+            numbers[i] = numbers[mindex];
+            numbers[mindex] = swap;
+        }
+    }else if(o == 0){
+        for (i = 0; i < numbers.size(); ++i) {
+            mindex = i;
+            for (j = i + 1; j < numbers.size(); ++j) {
+                if(numbers[mindex] < numbers[j]){
+                    mindex = j;
+                }
+            }
+            int swap = numbers[i];
+            numbers[i] = numbers[mindex];
+            numbers[mindex] = swap;
+        }
+    }
+
+    return numbers;
+}
+
+std::vector<int> selectionSort(std::vector<int> &numbers)
+{
+    int i, j, mindex;
+    for (i = 0; i < numbers.size(); ++i) {
+        mindex = i;
+        for (j = i + 1; j < numbers.size(); ++j) {
+            if(numbers[mindex] > numbers[j]){
+                mindex = j;
+            }
+        }
+        int swap = numbers[i];
+        numbers[i] = numbers[mindex];
+        numbers[mindex] = swap;
+    }
+
+    return numbers;
+}
+
+int* selectionSort(int array[], int size, int o)
+{
+    int i, j, mindex;
+    if(o == 1){
+        for (i = 0; i < size; ++i) {
+            mindex = i;
+            for (j = i + 1; j < size; ++j) {
+                if(array[mindex] > array[j]){
+                    mindex = j;
+                }
+            }
+            int swap = array[i];
+            array[i] = array[mindex];
+            array[mindex] = swap;
+        }
+    }else if(o == 0){
+        for (i = 0; i < size; ++i) {
+            mindex = i;
+            for (j = i + 1; j < size; ++j) {
+                if(array[mindex] < array[j]){
+                    mindex = j;
+                }
+            }
+            int swap = array[i];
+            array[i] = array[mindex];
+            array[mindex] = swap;
+        }
+    }
+
+    return array;
+}
+
+int* selectionSort(int array[], int size)
+{
+    int i, j, mindex;
+    for (i = 0; i < size; ++i) {
+        mindex = i;
+        for (j = i + 1; j < size; ++j) {
+            if(array[mindex] > array[j]){
+                mindex = j;
+            }
+        }
+        int swap = array[i];
+        array[i] = array[mindex];
+        array[mindex] = swap;
+    }
+
+    return array;
+}
---
+++ src/sort_functions.h
@@ -0,0 +1,13 @@
+#ifndef COMPUTER_VISION_SORT_FUNCTIONS_H
+#define COMPUTER_VISION_SORT_FUNCTIONS_H
+
+#include <vector>
+#include <iostream>
+
+std::vector<int> selectionSort(std::vector<int> &numbers, int o);
+std::vector<int> selectionSort(std::vector<int> &numbers);
+int* selectionSort(int array[], int size, int o);
+int* selectionSort(int array[], int size);
+
+
+#endif
